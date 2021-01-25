6eb3e1a1aae8cee007085bf85430a3b1c2e25b86
Tjøl Ravndal <tjol.ravndal@gmail.com>
2021-01-25 14:58:35 +0100

    EX1-1: task 1 moving helicopter sprite

--- ex1/core/src/tjol/ex1/game/Ex1Game.java
+++ ex1/core/src/tjol/ex1/game/Ex1Game.java
@@ -6,28 +6,36 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+import tjol.ex1.game.states.PlayState;
+
 public class Ex1Game extends ApplicationAdapter {
-	SpriteBatch batch;
+	public static final String TITLE = "Helicopter fun";
+
+	SpriteBatch sb;
 	Texture img;
-	
+	private PlayState playState;
+
+
+
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
+		sb = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		playState = new PlayState();
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		playState.update(Gdx.graphics.getDeltaTime());
+		playState.render(sb);
 	}
 	
 	@Override
 	public void dispose () {
-		batch.dispose();
+		sb.dispose();
 		img.dispose();
 	}
 }
